cd442a64e91bde418a3e2f3782d6cc5194e3e549
Eric Gurinowitsch <egurinowitsch@Erics-MacBook-Pro-2.local>
2020-07-13 14:21:48 -0400

    Added custom validation for year and name field

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -18,8 +18,27 @@ export class MediaItemFormComponent implements OnInit {
                 Validators.pattern('[\\w\\-\\s\\/]+')
             ])),
             category: new FormControl(''),
-            year: new FormControl('')
+            year: new FormControl('', this.yearValidator)
         });
+    }
+
+    yearValidator(control: FormControl) {
+        if (control.value.trim().length === 0) {
+            return null;
+        }
+        const year = parseInt(control.value, 10);
+        const minYear = 1800;
+        const maxYear = 2200;
+        if (year >= minYear && year <= maxYear) {
+            return null;
+        } else {
+            return { 
+                year: {
+                    min: minYear,
+                    max: maxYear
+                } 
+            };
+        }
     }
 
     onSubmit(mediaItem) {
