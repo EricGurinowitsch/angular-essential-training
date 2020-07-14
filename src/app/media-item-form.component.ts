8a816e210fe83d94964872c5b8643b110362b820
Eric Gurinowitsch <egurinowitsch@Erics-MacBook-Pro-2.local>
2020-07-14 09:03:48 -0400

    Setting up Angular HTTP module and using a mock for data to get post and delete

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -49,6 +49,7 @@ export class MediaItemFormComponent implements OnInit {
 
     onSubmit(mediaItem) {
         console.log("mediaItem: ", mediaItem);
-        this.mediaItemService.add(mediaItem);
+        this.mediaItemService.add(mediaItem)
+        .subscribe();
     }
 }
